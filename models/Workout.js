5c0728d6d1e670b089ee06c74d6f262f6f971d0b
Dbrandow332 <119837275+Dbrandow332@users.noreply.github.com>
2024-12-20 01:15:23 -0500

    Updated 12/20

--- models/Workout.js
+++ models/Workout.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const workoutSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Link to User
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     exercise: { type: String, required: true },
     calories: { type: Number, required: true },
-    duration: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
+    duration: { type: Number, required: true }, // in minutes
+    date: { type: Date, default: Date.now },    // optional, defaults to current date
 });
 
 module.exports = mongoose.model("Workout", workoutSchema);
